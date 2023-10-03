7f6519361876e32aaaddc2e56e37d60fb3860b9c
Anil <anilkumawat7896@gmail.com>
2023-10-04 01:45:21 +0530

    conversion

---
+++ 01_basics/03_covertionOperations.js
@@ -0,0 +1,31 @@
+/* let a = 2
+
+console.log(a+a);
+
+
+console.log(a*a)
+
+console.log(a-a);
+
+console.log(a**5);   // this will give a to the poser 5 
+console.log(a%a); */
+
+
+/* 
+
+string concet
+
+
+*/
+
+/* let str1 = "hellow";
+
+let str2 = " world";
+
+console.log(str1 + str2); */  // hellow world will be output here 
+
+console.log(2+"2");
+console.log("2"+2);
+console.log(1+2+"3");
+console.log("1"+2+3);
+
