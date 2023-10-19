aa8772690b98eb2336522ff3517dc86b1313085b
Anil <anilkumawat7896@gmail.com>
2023-10-19 09:56:56 +0530

    added

--- Projects/Project 3/script.js
+++ Projects/Project 3/script.js
@@ -1,7 +1,6 @@
 const clock = document.getElementById("clock");
 
 let date = new Date();
-console.log(date.toLocaleTimeString());
 
 setInterval(function () {
   clock.innerHTML = date.toLocaleTimeString();
